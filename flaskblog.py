9d2153c2974c7c6169b12692ce502bc6fc5b9690
Kartik Ramesh <kartikx2000@gmail.com>
2020-05-27 19:04:03 +0530

    Using Render_template instead of pure HTML code

--- flaskblog.py
+++ flaskblog.py
@@ -1,25 +1,33 @@
-from flask import Flask
-'''
-__name__ is a special variable in python
-that stores the name of the module
-'''
-app = Flask(__name__)
+from flask import Flask, render_template
 
-'''
-route is a complicated function, that is
-recommended to dive deeper into only much later.
-It handles much of the complicated backend. In
-essence, it allows us to create a function, that
-executes on our website, on the particular route
-provided to it
-'''
+posts = [
+    {
+        'author': 'Kartik Ramesh',
+        'title': 'First Post',
+        'content': 'This is my first post, welcome to my blog!',
+        'date_posted': 'May 27, 2020'
+    },
+    {
+        'author': 'Kartik Ramesh',
+        'title': 'Second Post',
+        'content': 'This is my second post!',
+        'date_posted': 'May 29, 2020'
+    }
+]
+
+app = Flask(__name__)
 
 
 @app.route('/')  # / is the root page
-@app.route('/home') #it's easy to stack up routes similar to switch cases
+@app.route('/home')  # it's easy to stack up routes similar to switch cases
 def hello_world():
-    return '<h1> Hello, Kartik! </h1>'
+    return render_template('home.html', posts=posts)
+
 
 @app.route('/about')
 def about_me():
-    return '<h2> Hello! </h2> I\'m a programmer from Faridabad!'
+    return render_template('about.html', title = "About Page")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
