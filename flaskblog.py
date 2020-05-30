125ac9cbefde79375f51ff408bc1bca8759a063f
Kartik Ramesh <kartikx2000@gmail.com>
2020-05-30 19:29:20 +0530

    New headers

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 posts = [
     {
