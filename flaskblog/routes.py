468e6e56eb60e8c7aa3f6504865d93c8a5f20ce2
Kartik Ramesh <kartikx2000@gmail.com>
2020-06-07 21:20:55 +0530

    Route method to update Post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -157,3 +157,26 @@ def post(post_id):
     # Else it will redirect to a 404 page.
     post = Post.query.get_or_404(post_id);
     return render_template("post.html", title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+def update_post(post_id):
+    form = UpdatePostForm()
+    post = Post.query.get_or_404(post_id)
+    
+    if post.author != current_user:
+        # This is an error for unauthorized access.
+        abort(403)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        # No need to add it to db, as it's already there.
+        db.session.commit()
+        flash("Your post has been edited successfully!", "success")
+        return redirect(url_for("post", post_id=post_id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template("create_post.html", title="Update Post", form=form,
+                            legend="Update Post")
+
