3f1960a03c8adbe206ca9463f78b60412682f39f
Kartik Ramesh <kartikx2000@gmail.com>
2020-06-07 22:00:35 +0530

    Delete Route implementation

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -181,3 +181,18 @@ def update_post(post_id):
     return render_template("create_post.html", title="Update Post", form=form,
                             legend="Update Post")
 
+
+# ? This takes in only a POST Request because you should only
+# ? be able to reach this route by filling in the modal.
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.commit()
+    flash("Your post has been successfully deleted!", "success")
+    return redirect(url_for("home"))
+
+
