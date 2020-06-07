d1219c31c5568948b30e27fdfc0d8b25f5cf3559
Kartik Ramesh <kartikx2000@gmail.com>
2020-06-07 17:20:36 +0530

    Implementation to save Image to File Directory

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -92,6 +92,28 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
+def save_picture(form_picture):
+
+    #Randomizing file name, to prevent overwrites.
+    # ? Is this correct? Isn't there still a possibility to overwrite?
+    f_name = secrets.token_hex(8)
+
+    # Using an unnamed variable as splitext returns the fname and the ext
+    _, f_ext = os.path.splitext(form_picture.filename)
+
+    picture_fn = f_name + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profilepics', picture_fn)
+    
+    # Resizing the image to save space
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    # Instead of saving the original picture,
+    # Save the resized picture instead.
+    i.save(picture_path)
+    return picture_fn
+
 @app.route("/account", methods=["GET", "POST"])
 @login_required # to be able to use this, you need to add a login_view to login_manager.
 def account():
