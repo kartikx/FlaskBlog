da377be2474bf5e3683150ea9e59f5208f06f3a0
Kartik Ramesh <kartikx2000@gmail.com>
2020-06-07 21:22:16 +0530

    Include update post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ import os
 import secrets
 from flaskblog import app, db, bcrypt
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, CreatePostForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, CreatePostForm, UpdatePostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
 from is_safe_url import is_safe_url
